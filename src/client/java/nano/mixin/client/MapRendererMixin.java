f2e84c54d0b2bd4f0e43f3833becc682d0aa8afa
DESKTOP-EOHR4LG\Alex <alexdechev1@gmail.com>
2025-07-30 10:17:06 -0700

    Version 1.4.0 For Minecraft 1.21.8

--- src/client/java/nano/mixin/client/MapRendererMixin.java
+++ src/client/java/nano/mixin/client/MapRendererMixin.java
@@ -2,90 +2,94 @@ package nano.mixin.client;
 
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.MapRenderState;
+import net.minecraft.client.render.MapRenderer;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.map.MapDecoration;
-import net.minecraft.item.map.MapDecorationType;
-import net.minecraft.item.map.MapDecorationTypes;
-import net.minecraft.item.map.MapState;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Colors;
 import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 
 @Environment(EnvType.CLIENT)
-@Mixin(targets = "net.minecraft.client.render.MapRenderer$MapTexture")
+@Mixin(MapRenderer.class)
 public class MapRendererMixin
 {
-	@Shadow private MapState state;
-
-	/**
-	 * Do not allow vanilla rendering of player icons.
-	 */
-	@ModifyVariable(method = "draw", at = @At("HEAD"), ordinal = 0)
-	private boolean hidePlayerIconsModifyVariable(boolean hidePlayerIcons)
-	{
-		return true;
-	}
-
 	/**
 	 * Draw custom player icons.
 	 */
-	@Inject(method = "draw", at = @At(value = "TAIL"))
-	private void drawInject(MatrixStack matrices, VertexConsumerProvider vertexConsumers, boolean hidePlayerIcons, int light, CallbackInfo info)
+	@Inject(method = "draw", at = @At(value = "HEAD"), cancellable = true)
+	private void drawInject(MapRenderState state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, boolean hidePlayerIcons, int light, CallbackInfo info)
 	{
 		MinecraftClient client = MinecraftClient.getInstance();
-		int layer = 0;
-		
-		// Ensure player icons are drawn on top of other icons.
-		for(MapDecoration mapDecoration : state.getDecorations())
-		{
-			RegistryEntry<MapDecorationType> decorationType = mapDecoration.type();
-			
-			if(decorationType != MapDecorationTypes.PLAYER && decorationType != MapDecorationTypes.PLAYER_OFF_LIMITS && decorationType != MapDecorationTypes.PLAYER_OFF_MAP)
-				layer++;
-		}
-		
-		for(MapDecoration mapDecoration : state.getDecorations())
-		{
-			RegistryEntry<MapDecorationType> decorationType = mapDecoration.type();
+		Matrix4f matrix4f = matrices.peek().getPositionMatrix();
+		VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getText(state.texture));
+		vertexConsumer.vertex(matrix4f, 0.0F, 128.0F, -0.01F).color(Colors.WHITE).texture(0.0F, 1.0F).light(light);
+		vertexConsumer.vertex(matrix4f, 128.0F, 128.0F, -0.01F).color(Colors.WHITE).texture(1.0F, 1.0F).light(light);
+		vertexConsumer.vertex(matrix4f, 128.0F, 0.0F, -0.01F).color(Colors.WHITE).texture(1.0F, 0.0F).light(light);
+		vertexConsumer.vertex(matrix4f, 0.0F, 0.0F, -0.01F).color(Colors.WHITE).texture(0.0F, 0.0F).light(light);
+		int i = 0;
 
-			if(decorationType != MapDecorationTypes.PLAYER && decorationType != MapDecorationTypes.PLAYER_OFF_LIMITS && decorationType != MapDecorationTypes.PLAYER_OFF_MAP)
-				continue;
-
+		for(MapRenderState.Decoration decoration : state.decorations)
+		{
 			matrices.push();
-			matrices.translate(0.0f + (float) mapDecoration.x() / 2.0f + 64.0f, 0.0f + (float) mapDecoration.z() / 2.0f + 64.0f, -0.02f);
-			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees((float) (mapDecoration.rotation() * 360) / 16.0f));
-			matrices.scale(4.0f, 4.0f, 3.0f);
-			matrices.translate(-0.125f, 0.125f, 0.0f);
-			Sprite sprite = client.getMapDecorationsAtlasManager().getSprite(mapDecoration);
-            float minU = sprite.getMinU();
-            float minV = sprite.getMinV();
-            float maxU = sprite.getMaxU();
-            float maxV = sprite.getMaxV();
-			int color = mapDecoration.name().get() != null ? Integer.valueOf(mapDecoration.name().get().getString()) : 0xFFFFFF;
-			int r = ColorHelper.Argb.getRed(color);
-			int b = ColorHelper.Argb.getBlue(color);
-			int g = ColorHelper.Argb.getGreen(color);
+			matrices.translate(decoration.x / 2.0F + 64.0F, decoration.z / 2.0F + 64.0F, -0.02F);
+			matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(decoration.rotation * 360 / 16.0F));
+			matrices.scale(4.0F, 4.0F, 3.0F);
+			matrices.translate(-0.125F, 0.125F, 0.0F);
 			Matrix4f matrix4f2 = matrices.peek().getPositionMatrix();
-			VertexConsumer vertexConsumer2 = vertexConsumers.getBuffer(RenderLayer.getText(sprite.getAtlasId()));
-			vertexConsumer2.vertex(matrix4f2, -1.0f, 1.0f, (float) layer * -0.001f).color(r, g, b, 255).texture(minU, minV).light(light);
-			vertexConsumer2.vertex(matrix4f2, 1.0f, 1.0f, (float) layer * -0.001f).color(r, g, b, 255).texture(maxU, minV).light(light);
-			vertexConsumer2.vertex(matrix4f2, 1.0f, -1.0f, (float) layer * -0.001f).color(r, g, b, 255).texture(maxU, maxV).light(light);
-			vertexConsumer2.vertex(matrix4f2, -1.0f, -1.0f, (float) layer * -0.001f).color(r, g, b, 255).texture(minU, maxV).light(light);
-			matrices.pop();
-			layer++;
+			Sprite sprite = decoration.sprite;
+			
+			if(sprite != null)
+			{
+				int color = Colors.WHITE;
+				
+				if(decoration.name != null && decoration.name.getString().contains("COLOR:"))
+				{
+					String[] split = decoration.name.getString().split(":");
+					
+					if(split.length > 1)
+						color = Integer.valueOf(split[1]);
+				}
+				
+				int r = ColorHelper.getRed(color);
+				int b = ColorHelper.getBlue(color);
+				int g = ColorHelper.getGreen(color);
+				VertexConsumer vertexConsumer2 = vertexConsumers.getBuffer(RenderLayer.getText(sprite.getAtlasId()));
+				vertexConsumer2.vertex(matrix4f2, -1.0F, 1.0F, i * -0.001F).color(r, g, b, 255).texture(sprite.getMinU(), sprite.getMinV()).light(light);
+				vertexConsumer2.vertex(matrix4f2, 1.0F, 1.0F, i * -0.001F).color(r, g, b, 255).texture(sprite.getMaxU(), sprite.getMinV()).light(light);
+				vertexConsumer2.vertex(matrix4f2, 1.0F, -1.0F, i * -0.001F).color(r, g, b, 255).texture(sprite.getMaxU(), sprite.getMaxV()).light(light);
+				vertexConsumer2.vertex(matrix4f2, -1.0F, -1.0F, i * -0.001F).color(r, g, b, 255).texture(sprite.getMinU(), sprite.getMaxV()).light(light);
+				matrices.pop();
+			}
+
+			if(decoration.name != null && !decoration.name.getString().contains("COLOR:"))
+			{
+				TextRenderer textRenderer = client.textRenderer;
+				float f = textRenderer.getWidth(decoration.name);
+				float g = MathHelper.clamp(25.0F / f, 0.0F, 6.0F / 9.0F);
+				matrices.push();
+				matrices.translate(decoration.x / 2.0F + 64.0F - f * g / 2.0F, decoration.z / 2.0F + 64.0F + 4.0F, -0.025F);
+				matrices.scale(g, g, -1.0F);
+				matrices.translate(0.0F, 0.0F, 0.1F);
+				textRenderer.draw(decoration.name, 0.0F, 0.0F, -1, false, matrices.peek().getPositionMatrix(), vertexConsumers, TextRenderer.TextLayerType.NORMAL, Integer.MIN_VALUE, light);
+				matrices.pop();
+			}
+
+			i++;
 		}
+		
+		info.cancel();
 	}
 }
--- src/main/java/nano/mixin/common/MapStateMixin.java
+++ src/main/java/nano/mixin/common/MapStateMixin.java
@@ -3,20 +3,28 @@ package nano.mixin.common;
 import java.util.List;
 import java.util.function.Predicate;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import com.google.common.collect.Lists;
 
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.map.MapDecorationType;
+import net.minecraft.item.map.MapDecorationTypes;
 import net.minecraft.item.map.MapState;
 import net.minecraft.item.map.MapState.PlayerUpdateTracker;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.WorldAccess;
 
 @Mixin(MapState.class)
 public class MapStateMixin
@@ -33,28 +41,52 @@ public class MapStateMixin
 	}
 	
 	/**
-	 * Do not render framed map icons because they look too similar to players on a green team.
+	 * Required to track players while in a frame.
 	 */
 	@Redirect(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isInFrame()Z"))
 	private boolean isInFrameRedirect(ItemStack stack)
 	{
 		return false;
 	}
+	
+	/**
+	 * Do not render framed map icons because they look too similar to players on a green team.
+	 */
+	@Inject(method = "addDecoration", at = @At(value = "HEAD"), cancellable = true)
+	private void addDecorationRedirect(RegistryEntry<MapDecorationType> type, @Nullable WorldAccess world, String key, double x, double z, double rotation, @Nullable Text text, CallbackInfo info)
+	{
+		if(type == MapDecorationTypes.FRAME)
+			info.cancel();
+	}
 
 	/**
-	 * Embed team color information in the text field.
+	 * Embed color information in the text field.
 	 */
-	@ModifyArgs(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;addDecoration(Lnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/world/WorldAccess;Ljava/lang/String;DDDLnet/minecraft/text/Text;)V", ordinal = 0))
+	@ModifyArgs(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/map/MapState;addDecoration(Lnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/world/WorldAccess;Ljava/lang/String;DDDLnet/minecraft/text/Text;)V"))
 	private void addIconModifyArgs(Args args)
 	{
+		RegistryEntry<MapDecorationType> type = args.get(0);
 		String name = args.get(2);
+		int defaultColorIndex = 15;
 		
-		for(PlayerUpdateTracker tracker : updateTrackers)
+		if(type == MapDecorationTypes.PLAYER)
 		{
-			if(tracker.player.getName().getString().equals(name))
+			for(PlayerUpdateTracker tracker : updateTrackers)
 			{
-				args.set(6, Text.of(String.valueOf(tracker.player.getTeamColorValue())));
-				break;
+				if(tracker.player.getName().getString().equals(name))
+				{
+					if(tracker.player.getScoreboardTeam() != null)
+						args.set(6, Text.of("COLOR:" + String.valueOf(tracker.player.getTeamColorValue())));
+					else
+						args.set(6, Text.of("COLOR:" + String.valueOf(Formatting.byColorIndex(defaultColorIndex).getColorValue())));
+					
+					break;
+				}
+				
+				defaultColorIndex++;
+				
+				if(defaultColorIndex > 15)
+					defaultColorIndex = 0;
 			}
 		}
 	}
